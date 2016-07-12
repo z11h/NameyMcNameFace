edd70d33d48c0ed12a6f3e5b8e94ade2b1d9d9d7
Zakaria Ridouh <zridouh@live.com>
2016-07-12 01:13:08 -0700

    formatting

--- namey.js
+++ namey.js
@@ -1,10 +1,10 @@
 /*
-* Sorce Code for NameyMcNameFace
-* Written by Zakaria Ridouh
-* Started Developement: 7/10/16
-* Latest change date: 7/10/16
-* Latest version number: 0.0.1
-*/
+ * Sorce Code for NameyMcNameFace
+ * Written by Zakaria Ridouh
+ * Started Developement: 7/10/16
+ * Latest change date: 7/10/16
+ * Latest version number: 0.0.1
+ */
 
 "use strict";
 
@@ -19,7 +19,13 @@ function setLName() {
 }
 
 function setPass() { // generates and sets password as well as password verification
-  document.getElementById('Password').value = chance.string({length: chance.integer({min: 8, max: 15}), pool: PASS_POOL});
+  document.getElementById('Password').value = chance.string({
+    length: chance.integer({
+      min: 8,
+      max: 15
+    }),
+    pool: PASS_POOL
+  });
 }
 
 function setCountry() { // The MS sign up page takes area codes, we only want the BR supported country (US)
@@ -27,15 +33,15 @@ function setCountry() { // The MS sign up page takes area codes, we only want th
 }
 
 function setGender() { // takes 'm' for male, 'f' for female, and 'u' for not specified
-  switch(chance.gender()) {
+  switch (chance.gender()) {
     case 'Male':
-        gender = 'm';
-        break;
+      gender = 'm';
+      break;
     case 'Female':
-        gender = 'f';
-        break;
+      gender = 'f';
+      break;
     default: // todo: actually give this a chance to be
-        gender = 'u';
+      gender = 'u';
   }
   document.getElementById('Gender').value = gender;
 }
@@ -45,7 +51,9 @@ function setEmailOptin() { // so accounts' Outlook email is only BR offers, no j
 }
 
 function setBirthMonth() { // generate a random parsed, trimed int for the month
-  document.getElementById('BirthMonth').value = parseInt(chance.month({raw: true}).numeric.trim()); // todo: only get numeric, not whole object
+  document.getElementById('BirthMonth').value = parseInt(chance.month({
+    raw: true
+  }).numeric.trim()); // todo: only get numeric, not whole object
 }
 
 function setBirthDay() { // generate a random unparsed day
@@ -53,7 +61,10 @@ function setBirthDay() { // generate a random unparsed day
 }
 
 function setBirthYear() { // generate a random unparsed year
-  document.getElementById('BirthYear').value = chance.year({min: 1955, max: 1998});
+  document.getElementById('BirthYear').value = chance.year({
+    min: 1955,
+    max: 1998
+  });
 }
 
 function setPhoneCountry() {
@@ -61,7 +72,11 @@ function setPhoneCountry() {
 }
 
 function setPhoneNumber() {
-  document.getElementById('PhoneNumber').value = chance.phone({ formatted: false, country: "us", mobile: true });
+  document.getElementById('PhoneNumber').value = chance.phone({
+    formatted: false,
+    country: "us",
+    mobile: true
+  });
 }
 
 function nameyMcNameFace() { // not best function name
