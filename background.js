72e7b494ec3a7f0417eaa11004f271ff8b37e8b6
Zakaria Ridouh <zridouh@live.com>
2016-07-13 13:58:04 -0700

    pass in init function as callback

--- background.js
+++ background.js
@@ -6,6 +6,8 @@ chrome.tabs.onUpdated.addListener((tabId, changeInfo) => {
 
 chrome.pageAction.onClicked.addListener(tab => {
     chrome.tabs.executeScript(tab.id, {file: "chance.min.js"}, () => {
-        chrome.tabs.executeScript(tab.id, {file: "namey.js"});
+        chrome.tabs.executeScript(tab.id, {file: "namey.js"}, ()=> {
+            chrome.tabs.executeScript(tab.id, {code: "nameyMcNameFace();"});
+        });
     });
 });
