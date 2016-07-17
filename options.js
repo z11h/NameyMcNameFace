e0eb79fb8a5e2feac6b44cc6812ba05b48d878fa
Zakaria Ridouh <zridouh@live.com>
2016-07-17 13:42:42 -0700

    more work on options

--- options.js
+++ options.js
@@ -1,1 +1,12 @@
 "use strict";
+
+let status = document.getElementById('status');
+
+function updateStatus(message) { //updates the status with a provided string.
+  status.innerHTML = message;
+}
+
+function saveSettings() {
+  // save the options to local storage
+  updateStatus("Successfully saved settings!");
+};
