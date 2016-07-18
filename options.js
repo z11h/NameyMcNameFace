831c4b401cbc372317a902072efed8c2a581678e
Zakaria Ridouh <zridouh@live.com>
2016-07-17 20:11:56 -0700

    start using sweetAlert2

--- options.js
+++ options.js
@@ -4,9 +4,17 @@ let status = document.getElementById('status');
 
 function updateStatus(message) { //updates the status with a provided string.
   status.innerHTML = message;
+  swal(
+    'Yay!',
+    'Successfully saved settings!',
+    'success'
+  )
 }
 
 function saveSettings() {
   // save the options to local storage
   updateStatus("Successfully saved settings!");
 };
+
+// attach event listeners
+document.getElementById('saveButton').addEventListener("click", saveSettings);
